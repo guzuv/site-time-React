07b87d5a39ce9deec872696686840bf6c0b2df0b
guzuv <110692178+guzuv@users.noreply.github.com>
2022-09-23 10:02:23 -0400

    Commit from TeleportHQ

--- src/views/home.js
+++ src/views/home.js
@@ -171,10 +171,22 @@ const Home = (props) => {
       </div>
       <div className="home-card-content4">
         <h3 className="home-text23 bigCard-Heading">minecraft.</h3>
-        <span className="home-text24 card-Text">
-          minecraft 1.5.2, running with multiplayer and voice chat! select
-          multiplayer and join with a friend! press escape when in server and
-          select nearby to enable voice chat.
+        <span className="home-text24">
+          <span className="card-Text">
+            minecraft 1.5.2, running with multiplayer and voice chat! select
+            multiplayer and join with a friend! press escape when in server and
+            select nearby to enable voice chat. It is
+            <span
+              dangerouslySetInnerHTML={{
+                __html: ' ',
+              }}
+            />
+          </span>
+          <span className="home-text26 card-Text">EXTREMELY BUGGY</span>
+          <span className="card-Text">
+            , if an error appears, try to refresh and try again in 30 minutes.
+          </span>
+          <br></br>
         </span>
         <a
           href="https://guzuv.repl.co/"
@@ -183,7 +195,7 @@ const Home = (props) => {
           className="home-link4 button"
         >
           <span>
-            <span className="home-text26">get mining!</span>
+            <span className="home-text30">get mining!</span>
             <br></br>
           </span>
         </a>
@@ -191,7 +203,7 @@ const Home = (props) => {
       <div className="home-section-separator1"></div>
       <div className="home-container2">
         <div className="home-testimonials">
-          <h2 className="home-text28">peeps i feature.</h2>
+          <h2 className="home-text32">peeps i feature.</h2>
           <div className="home-cards-container1">
             <div className="home-testimonial-card">
               <div className="home-card-heading">
@@ -241,7 +253,7 @@ const Home = (props) => {
                   </svg>
                 </div>
               </div>
-              <span className="home-text29">
+              <span className="home-text33">
                 the coolest nerd i know, check him out, he posts gaming and life
                 videos.
                 <span
@@ -299,7 +311,7 @@ const Home = (props) => {
                   </svg>
                 </div>
               </div>
-              <span className="home-text30">
+              <span className="home-text34">
                 good videos, does shorts, livestreams, and gameplay! dedicated
                 and great!
               </span>
@@ -350,7 +362,7 @@ const Home = (props) => {
                   </svg>
                 </div>
               </div>
-              <span className="home-text31">
+              <span className="home-text35">
                 <span>
                   loves roblox bedwars, has some nice vids, go say hi to him!
                 </span>
@@ -363,7 +375,7 @@ const Home = (props) => {
       <div className="home-container3"></div>
       <div id="contact" className="home-contact">
         <div className="home-content-container">
-          <h2 className="home-text34 section-Heading">Contact me!</h2>
+          <h2 className="home-text38 section-Heading">Contact me!</h2>
           <div className="home-locations-container">
             <div className="home-location-1">
               <span className="home-heading4">most active on discord.</span>
@@ -383,9 +395,9 @@ const Home = (props) => {
                 >
                   <path d="M585.143 512c0-80.571-65.714-146.286-146.286-146.286s-146.286 65.714-146.286 146.286 65.714 146.286 146.286 146.286 146.286-65.714 146.286-146.286zM664 512c0 124.571-100.571 225.143-225.143 225.143s-225.143-100.571-225.143-225.143 100.571-225.143 225.143-225.143 225.143 100.571 225.143 225.143zM725.714 277.714c0 29.143-23.429 52.571-52.571 52.571s-52.571-23.429-52.571-52.571 23.429-52.571 52.571-52.571 52.571 23.429 52.571 52.571zM438.857 152c-64 0-201.143-5.143-258.857 17.714-20 8-34.857 17.714-50.286 33.143s-25.143 30.286-33.143 50.286c-22.857 57.714-17.714 194.857-17.714 258.857s-5.143 201.143 17.714 258.857c8 20 17.714 34.857 33.143 50.286s30.286 25.143 50.286 33.143c57.714 22.857 194.857 17.714 258.857 17.714s201.143 5.143 258.857-17.714c20-8 34.857-17.714 50.286-33.143s25.143-30.286 33.143-50.286c22.857-57.714 17.714-194.857 17.714-258.857s5.143-201.143-17.714-258.857c-8-20-17.714-34.857-33.143-50.286s-30.286-25.143-50.286-33.143c-57.714-22.857-194.857-17.714-258.857-17.714zM877.714 512c0 60.571 0.571 120.571-2.857 181.143-3.429 70.286-19.429 132.571-70.857 184s-113.714 67.429-184 70.857c-60.571 3.429-120.571 2.857-181.143 2.857s-120.571 0.571-181.143-2.857c-70.286-3.429-132.571-19.429-184-70.857s-67.429-113.714-70.857-184c-3.429-60.571-2.857-120.571-2.857-181.143s-0.571-120.571 2.857-181.143c3.429-70.286 19.429-132.571 70.857-184s113.714-67.429 184-70.857c60.571-3.429 120.571-2.857 181.143-2.857s120.571-0.571 181.143 2.857c70.286 3.429 132.571 19.429 184 70.857s67.429 113.714 70.857 184c3.429 60.571 2.857 120.571 2.857 181.143z"></path>
                 </svg>
-                <span className="home-text36">    Guzuvy</span>
+                <span className="home-text40">    Guzuvy</span>
               </div>
-              <span className="home-text37">
+              <span className="home-text41">
                 <span>leo@guzuv.ml</span>
                 <br></br>
                 <br></br>
@@ -414,7 +426,7 @@ const Home = (props) => {
               data-thq="thq-dropdown-toggle"
               className="home-dropdown-toggle"
             >
-              <span className="home-text42">Sub-menu Item</span>
+              <span className="home-text46">Sub-menu Item</span>
             </div>
           </li>
           <li data-thq="thq-dropdown" className="home-dropdown1 list-item">
@@ -422,7 +434,7 @@ const Home = (props) => {
               data-thq="thq-dropdown-toggle"
               className="home-dropdown-toggle1"
             >
-              <span className="home-text43">Sub-menu Item</span>
+              <span className="home-text47">Sub-menu Item</span>
             </div>
           </li>
           <li data-thq="thq-dropdown" className="home-dropdown2 list-item">
@@ -430,7 +442,7 @@ const Home = (props) => {
               data-thq="thq-dropdown-toggle"
               className="home-dropdown-toggle2"
             >
-              <span className="home-text44">Sub-menu Item</span>
+              <span className="home-text48">Sub-menu Item</span>
             </div>
           </li>
         </ul>
