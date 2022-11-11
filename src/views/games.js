b939e7430039284dccbaef64e036b5e16ac0a203
guzuv <110692178+guzuv@users.noreply.github.com>
2022-11-11 09:23:53 -0500

    Commit from TeleportHQ

--- src/views/games.js
+++ src/views/games.js
@@ -134,25 +134,39 @@ const Games = (props) => {
               babies shall cry!
             </a>
           </div>
+          <div className="games-card-content07">
+            <h3 className="games-text17 BigCard-Heading">Basketball Random</h3>
+            <span className="games-text18">
+              two players, one device, play to 5 points, now with randomness!
+            </span>
+            <a
+              href="https://sra0obgp0bbi7ip2aqr2lcirgjd7tqck-a-sites-opensocial.googleusercontent.com/gadgets/ifr?url=https://sites.google.com/site/z001down/basket-random.xml&amp;container=enterprise&amp;view=default&amp;lang=en&amp;country=ALL&amp;sanitize=0&amp;v=419cc5c363609e21&amp;libs=core&amp;mid=22&amp;parent=https://sites.google.com/site/unblockedgame76/basket-random#st=e%3DAIHE3cDdLuhtOZhDgaHCSbx0lixygHcW0%252BSTp8mA0Xw5B%252Bq%252BN493WbkH74q6Z8xtqpr5cOIWiQeCeQ%252BlSDyBw%252BpehcEfVpu7Gcf3SardRsqmmqlS%252BG9%252FszQ%253D%26c%3Denterprise&amp;rpctoken=5086229488593983439"
+              target="_blank"
+              rel="noreferrer noopener"
+              className="games-link07 button"
+            >
+              ai or friend?
+            </a>
+          </div>
         </div>
         <div className="games-service-card1">
-          <div className="games-card-content07">
-            <h3 className="games-text17 BigCard-Heading">Doodle Jump.</h3>
-            <span className="games-text18 Card-Text">
+          <div className="games-card-content08">
+            <h3 className="games-text19 BigCard-Heading">Doodle Jump.</h3>
+            <span className="games-text20 Card-Text">
               jump as high as you can! can you beat 15000 score? good luck!
             </span>
             <a
               href="https://omackenzie.github.io/Doodle-Jump-in-JS/"
               target="_blank"
               rel="noreferrer noopener"
-              className="games-link07 Anchor button"
+              className="games-link08 Anchor button"
             >
               Get Jumping!
             </a>
           </div>
-          <div className="games-card-content08">
-            <h3 className="games-text19 BigCard-Heading">Doom!</h3>
-            <span className="games-text20 Card-Text">
+          <div className="games-card-content09">
+            <h3 className="games-text21 BigCard-Heading">Doom!</h3>
+            <span className="games-text22 Card-Text">
               get knee deep in hell, slaughter to victory! press any key to
               start. 
             </span>
@@ -160,14 +174,14 @@ const Games = (props) => {
               href="https://floooh.github.io/doom-sokol/"
               target="_blank"
               rel="noreferrer noopener"
-              className="games-link08 button Anchor"
+              className="games-link09 button Anchor"
             >
               Rip and Tear!
             </a>
           </div>
-          <div className="games-card-content09">
-            <h3 className="games-text21 BigCard-Heading">minecraft.</h3>
-            <span className="games-text22">
+          <div className="games-card-content10">
+            <h3 className="games-text23 BigCard-Heading">minecraft.</h3>
+            <span className="games-text24">
               <span>
                 minecraft 1.5.2, with multiplayer and voice chat! pick
                 multiplayer and join with a friend! press escape in-game and
@@ -179,16 +193,16 @@ const Games = (props) => {
               href="https://hyper-network.me/"
               target="_blank"
               rel="noreferrer noopener"
-              className="games-link09 button Anchor"
+              className="games-link10 button Anchor"
             >
               get mining!
             </a>
           </div>
-          <div className="games-card-content10">
-            <h3 className="games-text25 BigCard-Heading">
+          <div className="games-card-content11">
+            <h3 className="games-text27 BigCard-Heading">
               friday night funkin&apos;
             </h3>
-            <span className="games-text26">
+            <span className="games-text28">
               <span>friday night funkin, have fun!</span>
               <br></br>
             </span>
@@ -196,14 +210,14 @@ const Games = (props) => {
               href="https://funkin.guzuv.repl.co"
               target="_blank"
               rel="noreferrer noopener"
-              className="games-link10 button"
+              className="games-link11 button"
             >
               funkin time!
             </a>
           </div>
-          <div className="games-card-content11">
-            <h3 className="games-text29 BigCard-Heading">1v1.lol</h3>
-            <span className="games-text30">
+          <div className="games-card-content12">
+            <h3 className="games-text31 BigCard-Heading">1v1.lol</h3>
+            <span className="games-text32">
               <span>
                 play with friends, and duel players around the world using this!
               </span>
@@ -213,14 +227,14 @@ const Games = (props) => {
               href="https://1v1lol.56306.repl.co/"
               target="_blank"
               rel="noreferrer noopener"
-              className="games-link11 button"
+              className="games-link12 button"
             >
               fortnite?
             </a>
           </div>
-          <div className="games-card-content12">
-            <h3 className="games-text33 BigCard-Heading">run 3</h3>
-            <span className="games-text34">
+          <div className="games-card-content13">
+            <h3 className="games-text35 BigCard-Heading">run 3</h3>
+            <span className="games-text36">
               <span>
                 thanks bert for the idea! nostalgia trip and get all the dots!
               </span>
@@ -230,14 +244,14 @@ const Games = (props) => {
               href="https://lekug.github.io/tn6pS9dCf37xAhkJv/"
               target="_blank"
               rel="noreferrer noopener"
-              className="games-link12 button"
+              className="games-link13 button"
             >
               skater too cool.
             </a>
           </div>
-          <div className="games-card-content13">
-            <h3 className="games-text37 BigCard-Heading">pixel combat 2</h3>
-            <span className="games-text38">
+          <div className="games-card-content14">
+            <h3 className="games-text39 BigCard-Heading">pixel combat 2</h3>
+            <span className="games-text40">
               <span>
                 kudos to bert for the suggestion. Pixel combat 2, now with
                 multiplayer!
@@ -248,7 +262,7 @@ const Games = (props) => {
               href="https://debbiewatermanphd.com/uploads/5/0/5/6/5056555/custom_themes/607463927942590340/pixel-combat2.html"
               target="_blank"
               rel="noreferrer noopener"
-              className="games-link13 button"
+              className="games-link14 button"
             >
               <span>
                 <span>terrorist or not?</span>
